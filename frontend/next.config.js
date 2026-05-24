e7b7611d56569fa7d7e77f16f8634279d641d49a
taleshack-prog <talhack@yahoo.com>
2026-05-23 22:01:16 -0300

    fix: adiciona dominios Pinata no next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      'gateway.pinata.cloud',
+      'indigo-fascinating-lynx-313.mypinata.cloud',
+      'ipfs.io',
+      'cloudflare-ipfs.com',
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
